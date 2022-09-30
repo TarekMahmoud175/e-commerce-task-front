beba5e953e35a0f8c0cd02839bd1d120cce9c9e3
TarekMahmoud175 <tarek64@live.com>
2022-09-30 19:01:41 +0200

    ttesting some config

--- src/apis/Network.js
+++ src/apis/Network.js
@@ -1,41 +1,87 @@
 import DomainUrl from "./Domains";
 import axios from "axios";
 
-let headers = {}
+// let headers = {}
 
-const _axios = axios.create({
-  baseURL: DomainUrl,
-  headers
-})
+// const _axios = axios.create({
+//   baseURL: DomainUrl,
+//   headers
+// })
 
 
-_axios.interceptors.request.use(
-  async (config) => {
-    config.headers = {
-      ...config.headers,
-      'Content-Type': 'application/json'
+// _axios.interceptors.request.use(
+//   async (config) => {
+//     config.headers = {
+//       ...config.headers,
+//       'Content-Type': 'application/json'
+//     }
+//     console.log("config ===>", config)
+//     return config
+//   },
+//   (err) => {
+//     return Promise.reject(err?.response?.data)
+//   }
+// )
+
+
+// _axios.interceptors.response.use(
+//   response => {
+//     return response.data
+//   },
+//   err => {
+//     //backend server error
+//     if (err?.response?.data)
+//       return Promise.reject(err?.response?.data);
+//     //For cancellation
+//     if (err?.message)
+//       return Promise.reject(err);
+//   }
+// )
+
+// export default _axios
+
+
+
+
+export class Network {
+  constructor() {
+  }
+
+  static async fetch(url, init) {
+    const requestedUrl = DomainUrl + url;
+
+    console.log("requestedUrl", requestedUrl);
+    const response = await fetch(requestedUrl, {
+      mode: "cors",
+      ...init,
+      headers: Network.getHeaders(init.headers),
+    });
+    console.log("network response", response)
+
+    let promise;
+    if (![204, 201, 200].includes(response.status)) {
+      let promise = response.json().then((data) => {
+        return Promise.reject(data);
+      });
+      return promise.catch((error) => {
+        return Promise.reject(error);
+      });
     }
-    console.log("config ===>", config)
-    return config
-  },
-  (err) => {
-    return Promise.reject(err?.response?.data)
+    else if (response.status == 204) promise = Promise.resolve({});
+    else promise = response.json();
+
+    return promise;
   }
-)
 
+  static getHeaders(originalHeaders) {
+    let headers = {};
+    headers = {
+      ...headers,
+      ...originalHeaders,
+      "content-Type": "application/json",
+      Accept: "application/json",
+    };
 
-_axios.interceptors.response.use(
-  response => {
-    return response.data
-  },
-  err => {
-    //backend server error
-    if (err?.response?.data)
-      return Promise.reject(err?.response?.data);
-    //For cancellation
-    if (err?.message)
-      return Promise.reject(err);
+    return headers;
   }
-)
-
-export default _axios
+}
--- src/apis/Services/ProductsServices.js
+++ src/apis/Services/ProductsServices.js
@@ -1,20 +1,40 @@
 import { ProductEndPoints } from "../EndPoints/ProductsEndPoints";
-import _axios from "../Network";
+// import _axios from "../Network";
+import { Network } from "../Network";
 
 
 export class ProductServices {
 
+  // static getProducts() {
+  //   return _axios.get(ProductEndPoints.getProducts.url);
+  // }
+
+  // static AddProduct(data) {
+  //   return _axios.post(ProductEndPoints.addProduct.url, { ...data })
+  // }
+
+  // static deleteProducts(data) {
+  //   return _axios.post(ProductEndPoints.deleteProducts.url, { ...data })
+  // }
+
+
   static getProducts() {
-    return _axios.get(ProductEndPoints.getProducts.url);
+    return Network.fetch(ProductEndPoints.getProducts.url, {
+      method: ProductEndPoints.getProducts.method
+    })
   }
 
   static AddProduct(data) {
-    return _axios.post(ProductEndPoints.addProduct.url, { ...data })
+    return Network.fetch(ProductEndPoints.addProduct.url, {
+      method: ProductEndPoints.addProduct.method,
+      body: JSON.stringify(data),
+    })
   }
 
   static deleteProducts(data) {
-    console.log(data)
-    return _axios.post(ProductEndPoints.deleteProducts.url, { ...data })
+    return Network.fetch(ProductEndPoints.deleteProducts.url, {
+      method: ProductEndPoints.deleteProducts.method,
+      body: JSON.stringify(data),
+    })
   }
-
 }
