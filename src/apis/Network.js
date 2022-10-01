fed06dea2bcec5f34f2e69a2c21dbd36155bb02c
TarekMahmoud175 <tarek64@live.com>
2022-10-01 19:18:01 +0200

    handled network code and removed some comments

--- src/apis/Network.js
+++ src/apis/Network.js
@@ -1,47 +1,4 @@
 import DomainUrl from "./Domains";
-import axios from "axios";
-
-// let headers = {}
-
-// const _axios = axios.create({
-//   baseURL: DomainUrl,
-//   headers
-// })
-
-
-// _axios.interceptors.request.use(
-//   async (config) => {
-//     config.headers = {
-//       ...config.headers,
-//       'Content-Type': 'application/json'
-//     }
-//     console.log("config ===>", config)
-//     return config
-//   },
-//   (err) => {
-//     return Promise.reject(err?.response?.data)
-//   }
-// )
-
-
-// _axios.interceptors.response.use(
-//   response => {
-//     return response.data
-//   },
-//   err => {
-//     //backend server error
-//     if (err?.response?.data)
-//       return Promise.reject(err?.response?.data);
-//     //For cancellation
-//     if (err?.message)
-//       return Promise.reject(err);
-//   }
-// )
-
-// export default _axios
-
-
-
 
 export class Network {
   static async fetch(url, init) {
